1f5c3e17910e66a39b2ab5087097834c93be4a67
anilkumar0205 <anilkallagunta0205@gmail.com>
2023-11-27 17:39:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def bootstrap(request):
+    return render(request,'bootstrap.html')
+def bs1(request):
+    return render(request,'bs1.html')
